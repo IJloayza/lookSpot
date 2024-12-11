d9f2bec27359e46b1a864c294a634ad13525f733
Santiago Muñoz <2023_santiago.munoz@iticbcn.cat>
2024-12-11 19:58:31 +0100

    Added some dark mode switch

--- app/src/main/java/com/example/lookspot/Settings.kt
+++ app/src/main/java/com/example/lookspot/Settings.kt
@@ -1,12 +1,18 @@
 package com.example.lookspot
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Settings : AppCompatActivity() {
+
+    private lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +29,32 @@ class Settings : AppCompatActivity() {
                 menu.setPageElementAsActive(Menu.Page.SETTINGS)
             }
         }
+
+
+        // Obtener las preferencias compartidas
+        preferences = getSharedPreferences("AppSettings", MODE_PRIVATE)
+
+        // Verificar si el tema oscuro está habilitado desde SharedPreferences
+        val isDarkMode = preferences.getBoolean("dark_mode", false)
+
+        // Configurar el estado del switch según el tema actual
+        val darkModeSwitch: SwitchCompat = findViewById(R.id.themeMode)
+        darkModeSwitch.isChecked = isDarkMode
+
+        // Configurar el listener para el switch
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            // Si el estado del interruptor ha cambiado
+            if (isChecked) {
+                // Cambiar a modo oscuro
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                preferences.edit().putBoolean("dark_mode", true).apply()
+            } else {
+                // Cambiar a modo claro
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                preferences.edit().putBoolean("dark_mode", false).apply()
+            }
+        }
+
+
     }
 }
